94ba8f8be69ce51209c717b7630db517c7177be8
MUTAN Network <42528903+mutanpro@users.noreply.github.com>
2024-04-27 20:01:41 +0700

    Update playlists-channel.js

--- playlists-channel.js
+++ playlists-channel.js
@@ -1,20 +1,34 @@
-[
-  {
-"title": "SSC 2",
-                    "type": "dash",
-                    "file": 'https://ssc2-ak.akamaized.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd',
-                    "drm": {
-                        "clearkey": { "keyId": "8bcfc55359e24bd7ad1c5560a96ddd3c", "key": "b5dcf721ab522af92a9d3bf0bd55b596" }
-                    },
-                    "label": "0"
-                },
+[{
+        "title": "SSC 2",
+        "type": "dash",
+        "file": "https://ssc2-ak.akamaized.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd",
+        "drm": {
+            "clearkey": {
+                "keyId": "8bcfc55359e24bd7ad1c5560a96ddd3c",
+                "key": "b5dcf721ab522af92a9d3bf0bd55b596"
+            }
+        },
+        "label": "0"
+    },
     {
-      "file": "https://ssc5-ak.akamaized.net/out/v1/99289eac5a7b4319905da595afbd792b/index.mpd",
-      "title": "SSC 5",
-      "type": "dash",
-      "drm": {
-          "clearkey": { "keyId": "c88b512b17ab4f6cb09eb0ff4a1056ed", "key": "adc08ee1c20a734972a55c9aebbd1888" }
-      },
-      "label": "0"
+        "title": "SSC Extra 3",
+        "type": "dash",
+        "file": "https://ssc3-ak.akamaized.net/out/v1/42e86125555242aaa2a12056832e7814/index.mpd",
+        "drm": {
+            "clearkey": { "keyId": "7de5dd08ad8041d586c2f16ccc9490a1", "key": "5e1503f3398b34f5099933fedab847ef" }
+        },
+        "label": "0"
+    },
+    {
+        "file": "https://ssc5-ak.akamaized.net/out/v1/99289eac5a7b4319905da595afbd792b/index.mpd",
+        "title": "SSC Extra 5",
+        "type": "dash",
+        "drm": {
+            "clearkey": {
+                "keyId": "c88b512b17ab4f6cb09eb0ff4a1056ed",
+                "key": "adc08ee1c20a734972a55c9aebbd1888"
+            }
+        },
+        "label": "0"
     }
 ]
