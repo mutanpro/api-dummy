f108e66d2af457a71a902a18baecf11c0c5153dd
MUTAN Network <42528903+mutanpro@users.noreply.github.com>
2024-04-27 17:30:17 +0700

    Create playlists.js

---
+++ playlists.js
@@ -0,0 +1,20 @@
+var playlists = [
+    [{
+        file: "https://ssc1-ak.akamaized.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd",
+        title: "SSC",
+        type: "dash",
+        drm: {
+            "clearkey": { "keyId": "d84c325f36814f39bbe59080272b10c3", "key": "550727de4c96ef1ecff874905493580f" }
+        },
+        label: "0"
+    },{
+        file: "https://ssc5-ak.akamaized.net/out/v1/99289eac5a7b4319905da595afbd792b/index.mpd",
+        title: "SSC 5",
+        type: "dash",
+        drm: {
+            "clearkey": { "keyId": "c88b512b17ab4f6cb09eb0ff4a1056ed", "key": "adc08ee1c20a734972a55c9aebbd1888" }
+        },
+        label: "0"
+    }]
+    // Add more playlists as needed
+];
