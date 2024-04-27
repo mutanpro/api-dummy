4e6e3e1b3e8e180b828afe17bf2bd1823abea99a
MUTAN Network <42528903+mutanpro@users.noreply.github.com>
2024-04-27 20:11:51 +0700

    Update playlists-channel.js

--- playlists-channel.js
+++ playlists-channel.js
@@ -1,6 +1,6 @@
 [
     {
-
+        "title": "SPORT TV 1",
         "type": "dash",
         "file": "https://da-d50d12b5.online.meo.pt/wp/cdn-er-vspp-cvl2.online.meo.pt/shls/LIVE$SPORTTV1_HD/index.m3u8/S!d2EISU9TX0xpdmUSBkPIS1XDVP7...8BFgyf/Level(3000000)",
         "label": "0"
