9f6e383b39ed35c0d569d76b57349ee425e7da4f
MUTAN Network <42528903+mutanpro@users.noreply.github.com>
2024-04-27 19:18:24 +0700

    Update playlists-channel.js

--- playlists-channel.js
+++ playlists-channel.js
@@ -1,14 +1,5 @@
 [
-    {
-      file: "https://ssc1-ak.akamaized.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd",
-      title: "SSC",
-      type: "dash",
-      drm: {
-          "clearkey": { "keyId": "d84c325f36814f39bbe59080272b10c3", "key": "550727de4c96ef1ecff874905493580f" }
-      },
-      label: "0"
-    },
-    {
+  {
       file: "https://ssc5-ak.akamaized.net/out/v1/99289eac5a7b4319905da595afbd792b/index.mpd",
       title: "SSC 5",
       type: "dash",
@@ -17,4 +8,4 @@
       },
       label: "0"
     }
-  ]
+]
