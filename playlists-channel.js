108618db4a66b80a44c69ac804b0449c661228f9
MUTAN Network <42528903+mutanpro@users.noreply.github.com>
2024-04-28 11:06:14 +0700

    Update playlists-channel.js

--- playlists-channel.js
+++ playlists-channel.js
@@ -15,13 +15,16 @@
         "type": "dash",
         "file": "https://da-d50d12b5.online.meo.pt/wp/cdn-er-vspp-cvl2.online.meo.pt/shls/LIVE$SPORTTV1_HD/index.m3u8/S!d2EISU9TX0xpdmUSBkPIS1XDVP7...8BFgyf/Level(3000000)",
         "label": "0"
-      },
-      {
+    },
+    {
         "title": "Dazn F1",
         "type": "dash",
         "file": "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/cqbcvgkb83/out/v1/4dbe05ecfb1540448d82d68eeebfbb1c/cenc.mpd",
         "drm": {
-            "clearkey": { "keyId": "1061be12d303247426ec25e8369b2647", "key": "bd622b0e610295de3b0bccb850ccaaaa" }
+            "clearkey": {
+                "keyId": "1061be12d303247426ec25e8369b2647",
+                "key": "bd622b0e610295de3b0bccb850ccaaaa"
+            }
         },
         "label": "0"
     },
@@ -42,7 +45,10 @@
         "type": "dash",
         "file": "https://ssc3-ak.akamaized.net/out/v1/42e86125555242aaa2a12056832e7814/index.mpd",
         "drm": {
-            "clearkey": { "keyId": "7de5dd08ad8041d586c2f16ccc9490a1", "key": "5e1503f3398b34f5099933fedab847ef" }
+            "clearkey": {
+                "keyId": "7de5dd08ad8041d586c2f16ccc9490a1",
+                "key": "5e1503f3398b34f5099933fedab847ef"
+            }
         },
         "label": "0"
     },
@@ -58,22 +64,35 @@
         },
         "label": "0"
     },
-
-
     {
-                      "title": "ESPN",
-                    "type": "dash",
-                    "file": "https://chromecast.cvattv.com.ar/live/c7eds/Fox_Sports_Premiun_HD/SA_Live_dash_enc/Fox_Sports_Premiun_HD.mpd",
-                    "drm": {
-                        "clearkey": { "keyId": "4c230dbc7f6a4bfa6ad0aa73ff792374", "key": "4186a7c2a15f590a9399886feaec4257" }
-                    },
-                    "label": "0"
-                },
-    
+        "title": "ESPN",
+        "type": "dash",
+        "file": "https://chromecast.cvattv.com.ar/live/c7eds/Fox_Sports_Premiun_HD/SA_Live_dash_enc/Fox_Sports_Premiun_HD.mpd",
+        "drm": {
+            "clearkey": {
+                "keyId": "4c230dbc7f6a4bfa6ad0aa73ff792374",
+                "key": "4186a7c2a15f590a9399886feaec4257"
+            }
+        },
+        "label": "0"
+    },
+    {
+        "title": "USA TV",
+        "type": "dash",
+        "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-east)/master.mpd?",
+        "drm": {
+            "clearkey": {
+                "keyId": "882c9f5613b43b47adc70aa968a308ce",
+                "key": "de534ef8914bfe62ba3cdd6bdb9e1c04"
+            }
+        },
+        "label": "0"
+    },
     {
         "url": "https://sportsonline.si/channels/hd/hd2.php",
         "title": "HD 2"
-    },{
+    },
+    {
         "url": "https://sportsonline.si/channels/pt/eleven1.php",
         "title": "ELEVEN 1"
     }
